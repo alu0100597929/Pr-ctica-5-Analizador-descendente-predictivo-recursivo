9140a497330a54b130fb8378c637192091f2d75e
juanma3108 <alu0100597929@ull.edu.es>
2014-03-20 01:44:46 +0000

    test 18

--- public/tests/tests.js
+++ public/tests/tests.js
@@ -1,11 +1,10 @@
-
 var assert = chai.assert;
  
  suite('Pruebas: ', function() {
     test('Prueba 1', function() {
          original.value = "a = 2.";
          main();
- 		assert.equal(OUTPUT.innerHTML, '  {\n    "type": "=",\n    "left": {\n      "type": "ID",\n      "value": "a"\n    },\n    "right": {\n      "type": "NUM",\n      "value": 2\n    }\n  }');
+ 			assert.equal(OUTPUT.innerHTML, '  {\n    "type": "=",\n    "left": {\n      "type": "ID",\n      "value": "a"\n    },\n    "right": {\n      "type": "NUM",\n      "value": 2\n    }\n  }');
      });
 
 
